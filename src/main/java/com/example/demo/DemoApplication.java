0c5bd26a826b2df3f263c4c0d275ca5dfcfa627d
Aldiyar <aldikzhaks@gmail.com>
2025-08-11 19:19:49 +0500

    Spring boot: part 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
-
-import com.example.demo.config.ApplicationConfiguration;
-import com.example.demo.services.UserService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+@SpringBootApplication
 public class DemoApplication {
+
 	public static void main(String[] args) {
-		var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
-		var userService = context.getBean("userService", UserService.class);
-		System.out.println(userService);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
