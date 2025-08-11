f30b8094c2f5b0bdea5914f5e9e7a9d640dd7210
Aldiyar <aldikzhaks@gmail.com>
2025-08-11 19:03:56 +0500

    added: custom dev and prod conditional for bean creation

--- src/main/java/com/example/demo/config/ApplicationConfiguration.java
+++ src/main/java/com/example/demo/config/ApplicationConfiguration.java
@@ -1,12 +1,13 @@
 package com.example.demo.config;
 
+import com.example.demo.config.condition.devConditional;
+import com.example.demo.config.condition.prodConditional;
 import com.example.demo.pojo.User;
 import com.example.demo.repositories.UserRepository;
 import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
-import org.springframework.core.annotation.AliasFor;
 
 @Configuration
 @PropertySource("classpath:application.properties")
@@ -33,6 +34,18 @@ public class ApplicationConfiguration {
     @Bean
     public UserService userService(UserRepository userRepository1) {
         return new UserService(userRepository1);
+    }
+    @Bean
+    @Conditional(devConditional.class)
+    public User dev1(){
+        System.out.println("hello from dev");
+        return new User("Aldiyar","Maksutov","bober@mail.com",true);
+    }
+    @Bean
+    @Conditional(prodConditional.class)
+    public User prod1(){
+        System.out.println("hello from prod");
+        return new User("Madiyar","Maksutov","bober@mail.com",true);
     }
 
 }
