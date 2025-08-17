fc3a8d234e069b8d3579d82a73730db0921a0841
Aldiyar <aldikzhaks@gmail.com>
2025-08-17 19:59:29 +0500

    updated: UserService and UserRepo

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repositories;
 
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public class UserRepository {
-    public UserRepository(){
-    }
+public interface UserRepository extends JpaRepository<User, Long>{
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,8 +1,11 @@
 package com.example.demo.services;
 
+import com.example.demo.entity.User;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -13,8 +16,34 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    @Override
-    public String toString() {
-        return "Hello from UserService";
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User update(User user) {
+        return userRepository.findById(user.getId())
+                .map(existingUser -> {
+                    existingUser.setName(user.getName());
+                    existingUser.setEmail(user.getEmail());
+                    existingUser.setPassword(user.getPassword());
+                    return userRepository.save(existingUser);
+                })
+                .orElse(null);
+    }
+
+    public boolean delete(Long id) {
+        if(userRepository.existsById(id)){
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
