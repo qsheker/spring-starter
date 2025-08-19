fcadb0d6e632b575236fb973d48a84e66f03052b
Aldiyar <aldikzhaks@gmail.com>
2025-08-19 20:18:15 +0500

    updated: UserRepository

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -2,9 +2,14 @@ package com.example.demo.repositories;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+    @Query("select u from User as u where lower(u.name) like %:name%")
+    List<User> findByNameContainingIgnoreCase(@Param("name") String name);
+
 }
