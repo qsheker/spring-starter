5e022ea94de621325cec03856286ee057d773310
Aldiyar <aldikzhaks@gmail.com>
2025-08-24 14:51:42 +0500

    test(UserServiceTest): added method to test projection

--- src/test/java/com/example/demo/services/UserServiceTest.java
+++ src/test/java/com/example/demo/services/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 
 import com.example.demo.DemoApplication;
+import com.example.demo.entity.User;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,12 +10,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootTest(classes = DemoApplication.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserServiceTest {
 
+     static final Long ID = 1L;
+
     @Autowired
     private UserService userService;
+
+    @Test
+    void testDto(){
+        var user = userService.findUserById(ID);
+        assertEquals(true, user.isPresent());
+    }
 
     @Test
     void findAll() {
@@ -26,5 +38,13 @@ class UserServiceTest {
     void updateUserEmailById() {
         boolean updated = userService.updateUserEmailById(1L,"MaxWebber@gmail.com");
         assertEquals(false,updated);
+    }
+    @Test
+    void testOrders(){
+        List<User> userList = userService.findAllWithOrders();
+        Optional<User> user = userService.findUserWithOrdersById(ID);
+
+        assertEquals(3,userList.size());
+        assertEquals(true,user.isPresent());
     }
 }
