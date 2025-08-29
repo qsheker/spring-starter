6becac167423133c2b4afe1d173abfd27312f5ad
Aldiyar <aldikzhaks@gmail.com>
2025-08-29 12:25:50 +0500

    added: Product Repository

--- src/main/java/com/example/demo/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -1,4 +1,27 @@
 package com.example.demo.repositories;
 
-public interface ProductRepository {
+import com.example.demo.domain.entity.Category;
+import com.example.demo.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByNameContainingIgnoreCase(String name);
+
+    List<Product> findByCategory(Category category);
+
+    List<Product> findByPriceBetween(BigDecimal price, BigDecimal priceBefore);
+
+    List<Product> findByStockGreaterThan(Integer stockIsGreaterThan);
+
+    List<Product> findAllByOrderByPriceDesc();
+
+    List<Product> findAllByOrderByPriceAsc();
+
+    List<Product> findByCategoryAndPriceBetweenAndStockGreaterThan(Category category, BigDecimal priceAfter, BigDecimal priceBefore, Integer stockIsGreaterThan);
+
+
+
 }
