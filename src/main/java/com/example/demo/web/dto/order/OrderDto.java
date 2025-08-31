eb82cb8fad337cedd7277eef986c6ac1340b9015
Aldiyar <aldikzhaks@gmail.com>
2025-09-01 02:15:32 +0500

    added: order dto

--- src/main/java/com/example/demo/web/dto/order/OrderDto.java
+++ src/main/java/com/example/demo/web/dto/order/OrderDto.java
@@ -1,4 +1,19 @@
 package com.example.demo.web.dto.order;
 
-public class OrderDto {
+import com.example.demo.enums.OrderStatus;
+import com.example.demo.web.dto.orderItem.OrderItemDto;
+import com.example.demo.web.dto.user.UserShortDto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record OrderDto(
+        Long id,
+        LocalDateTime orderDate,
+        OrderStatus orderStatus,
+        BigDecimal totalPrice,
+        UserShortDto user,
+        List<OrderItemDto> items
+){
 }
