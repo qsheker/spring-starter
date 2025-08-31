812a32016f0cb64d742b28e1c8f5a09792149454
Aldiyar <aldikzhaks@gmail.com>
2025-09-01 02:16:26 +0500

    added: product dto

--- src/main/java/com/example/demo/web/dto/product/ProductDto.java
+++ src/main/java/com/example/demo/web/dto/product/ProductDto.java
@@ -1,4 +1,14 @@
 package com.example.demo.web.dto.product;
 
-public class ProductDto {
+import com.example.demo.web.dto.category.CategoryDto;
+
+import java.math.BigDecimal;
+
+public record ProductDto(
+        Long id,
+        String name,
+        String description,
+        BigDecimal price,
+        CategoryDto category
+) {
 }
