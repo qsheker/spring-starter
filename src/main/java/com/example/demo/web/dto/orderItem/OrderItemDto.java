0414479fc6f6b093982e566d34bfee5b9379ac68
Aldiyar <aldikzhaks@gmail.com>
2025-09-01 02:16:02 +0500

    added: Order Item Dto

--- src/main/java/com/example/demo/web/dto/orderItem/OrderItemDto.java
+++ src/main/java/com/example/demo/web/dto/orderItem/OrderItemDto.java
@@ -1,4 +1,13 @@
 package com.example.demo.web.dto.orderItem;
 
-public class OrderItemDto {
+import com.example.demo.web.dto.product.ProductDto;
+
+import java.math.BigDecimal;
+
+public record OrderItemDto(
+        Long id,
+        BigDecimal price,
+        Integer quantity,
+        ProductDto product
+) {
 }
