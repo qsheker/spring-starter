61060073e5f0f7c5a36f05e1a39b078c9a1a1c6d
Aldiyar <aldikzhaks@gmail.com>
2025-09-04 21:48:35 +0500

    updated: order item dto

--- src/main/java/com/example/demo/web/dto/orderItem/OrderItemDto.java
+++ src/main/java/com/example/demo/web/dto/orderItem/OrderItemDto.java
@@ -5,7 +5,6 @@ import com.example.demo.web.dto.product.ProductDto;
 import java.math.BigDecimal;
 
 public record OrderItemDto(
-        Long id,
         BigDecimal price,
         Integer quantity,
         ProductDto product
