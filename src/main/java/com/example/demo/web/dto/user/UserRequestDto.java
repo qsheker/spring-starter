ce45ee02eb1d5efdedae0b53e9bfd8b9f51b0d8f
Aldiyar <aldikzhaks@gmail.com>
2025-09-05 23:12:26 +0500

    added: UserRequestDto

--- src/main/java/com/example/demo/web/dto/user/UserRequestDto.java
+++ src/main/java/com/example/demo/web/dto/user/UserRequestDto.java
@@ -1,4 +1,12 @@
 package com.example.demo.web.dto.user;
 
-public class UserRequestDto {
+import com.example.demo.enums.Role;
+
+public record UserRequestDto(
+        Long id,
+        String name,
+        String email,
+        String password,
+        Role role
+) {
 }
