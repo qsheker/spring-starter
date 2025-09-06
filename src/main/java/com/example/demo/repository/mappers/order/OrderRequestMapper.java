4246b0e378e2096cb67f8028c5a025c903a8f9af
Aldiyar <aldikzhaks@gmail.com>
2025-09-06 16:42:37 +0500

    added: mappers for Order Request and Order Short DTO's

--- src/main/java/com/example/demo/repository/mappers/order/OrderRequestMapper.java
+++ src/main/java/com/example/demo/repository/mappers/order/OrderRequestMapper.java
@@ -1,4 +1,12 @@
 package com.example.demo.repository.mappers.order;
 
+import com.example.demo.domain.entity.Order;
+import com.example.demo.web.dto.order.OrderRequestDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface OrderRequestMapper {
+    OrderRequestDto toDto(Order order);
+
+    Order toEntity(OrderRequestDto orderDto);
 }
--- src/main/java/com/example/demo/repository/mappers/order/OrderShortMapper.java
+++ src/main/java/com/example/demo/repository/mappers/order/OrderShortMapper.java
@@ -1,4 +1,12 @@
 package com.example.demo.repository.mappers.order;
 
+import com.example.demo.domain.entity.Order;
+import com.example.demo.web.dto.order.OrderShortDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface OrderShortMapper {
+    OrderShortDto toDto(Order order);
+
+    Order toEntity(OrderShortDto orderDto);
 }
