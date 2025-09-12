da8f6ee398461b7a9fb64408b3a5942ebe4d36c9
Aldiyar <aldikzhaks@gmail.com>
2025-09-13 00:29:21 +0500

    updated: product mapper

--- src/main/java/com/example/demo/repository/mappers/product/ProductMapper.java
+++ src/main/java/com/example/demo/repository/mappers/product/ProductMapper.java
@@ -2,8 +2,10 @@ package com.example.demo.repository.mappers.product;
 
 import com.example.demo.domain.entity.Product;
 import com.example.demo.web.dto.product.ProductDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProductMapper{
     ProductDto toDto(Product product);
 
