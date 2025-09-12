da59c1130d2dfd1a6bf93d5f115f579b6b450a59
Aldiyar <aldikzhaks@gmail.com>
2025-09-13 00:12:50 +0500

    added: Product Short Dto

---
+++ src/main/java/com/example/demo/web/dto/product/ProductShortDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.web.dto.product;
+
+import lombok.Data;
+
+@Data
+public class ProductShortDto {
+    private Long id;
+    private String name;
+    private String description;
+}
