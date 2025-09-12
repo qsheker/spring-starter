e37ee36345c0c55f5e99299c82d940f39c5e3232
Aldiyar <aldikzhaks@gmail.com>
2025-09-13 00:08:12 +0500

    added: Order Item Controller

---
+++ src/main/java/com/example/demo/controller/OrderItemController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.repository.mappers.orderItem.OrderItemMapper;
+import com.example.demo.services.OrderItemService;
+import com.example.demo.web.dto.orderItem.OrderItemDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/orders/items")
+public class OrderItemController {
+    private final OrderItemMapper orderItemMapper;
+    private final OrderItemService orderItemService;
+
+    public OrderItemController(OrderItemMapper orderItemMapper, OrderItemService orderItemService) {
+        this.orderItemMapper = orderItemMapper;
+        this.orderItemService = orderItemService;
+    }
+
+    @GetMapping("{orderId}")
+    public List<OrderItemDto> getOrderItemByOrderId(@PathVariable("orderId") Long id){
+        return orderItemService.findByOrderId(id).stream()
+                .map(orderItem -> orderItemMapper.toDto(orderItem))
+                .toList();
+    }
+    @GetMapping("{productId}")
+    public List<OrderItemDto> getOrderItemByProductId(@PathVariable("productId") Long id){
+        return orderItemService.findByProductId(id).stream()
+                .map(orderItem -> orderItemMapper.toDto(orderItem))
+                .toList();
+    }
+
+}
