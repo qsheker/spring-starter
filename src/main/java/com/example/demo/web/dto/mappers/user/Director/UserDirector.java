bfd9674f99b35ecca368c2a469612d9044739377
Aldiyar <aldikzhaks@gmail.com>
2025-09-13 17:19:16 +0500

    feat: added user director that can create many dto's with specific fields

--- src/main/java/com/example/demo/web/dto/mappers/user/Director/UserDirector.java
+++ src/main/java/com/example/demo/web/dto/mappers/user/Director/UserDirector.java
@@ -1,13 +1,15 @@
-package com.example.demo.repository.mappers.user.Director;
+package com.example.demo.web.dto.mappers.user.Director;
 
 import com.example.demo.domain.entity.User;
 import com.example.demo.web.dto.user.UserTotalPriceDto;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@Component
 public class UserDirector {
     public UserTotalPriceDto createUserWithTotalPrice(BigDecimal totalPrice,User user){
-        return new UserTotalPriceDto().builder()
+        return UserTotalPriceDto.builder()
                 .id(user.getId())
                 .name(user.getName())
                 .email(user.getEmail())
